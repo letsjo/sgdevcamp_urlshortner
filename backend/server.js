6e83723658c37123d0536ceb908a7457aec4e914
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-12-20 19:50:06 +0900

    feat(server): Node.js와 mongoDB 연결

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(
+    'mongodb+srv://jeongwon:rose5612@cluster0.9j6oxxs.mongodb.net/?retryWrites=true&w=majority',
+    {
+      // useNewUrlPaser: true,
+      // useUnifiedTofology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 app.listen(8080, function(){
     console.log('Listening on port 8080')
