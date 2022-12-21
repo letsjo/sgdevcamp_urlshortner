379710c22b3b1833e29266ce0deb615d1d260bf2
letsjo <hyunoh.jo@gmail.com>
2022-12-21 17:42:17 +0900

    feat(urls): short Url 중복값 있을 경우 다시 생성으로 callback 처리

--- backend/routes/urls.js
+++ backend/routes/urls.js
@@ -1,54 +1,39 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const Url = require('../models/url');
+const Crypto = require('../utils/crypto');
 
-// Find All
-router.get('/', (req, res) => {
-  Url.findAll()
-    .then(urls => {
-      if (!urls.length) return res.status(404).send({ err: 'URL not found' });
-      res.send(`find successfully: ${urls}`);
-    })
+const createShortUrl = (res, req) => {
+  const shortUrl = Crypto.encoding(req.body.full);
+  Url.findOneByShortUrl(shortUrl).then(findUrl => {
+    if (!findUrl) {
+      console.log('shortUrl(in):', shortUrl);
+      return registerShortUrl(res, shortUrl, req.body.full);
+    }
+    return createShortUrl(res, req);
+  })
     .catch(err => res.status(500).send(err));
-});
+};
 
-// Find One by url id
-router.get('/urlid/:urlId', (req, res) => {
-  Url.findOneByUrlId(req.params.urlId)
+const registerShortUrl = (res, shortUrl, fullUrl) => {
+  Url.create({ full: fullUrl, short: shortUrl })
+    .then(url => res.send(url))
+    .catch(err => res.status(500).send(err));
+};
+
+// redirect by short url
+router.get('/:short', (req, res) => {
+  Url.findOneByShortUrl(req.params.short)
     .then(url => {
       if (!url) return res.status(404).send({ err: 'URL not found' });
-      res.send(`findOne successfully: ${url}`);
-    })
-    .catch(err => res.status(500).send(err));
-});
-
-router.get('/:urlId', (req, res) => {
-  Url.findOneByUrlId(req.params.urlId)
-    .then(url => {
-      if (!url) return res.status(404).send({ err: 'URL not found' });
-      res.send(`findOne successfully: ${url}`);
+      res.redirect(url.full);
     })
     .catch(err => res.status(500).send(err));
 });
 
 // Create new url document
 router.post('/', (req, res) => {
-  Url.create(req.body)
-    .then(url => res.send(url))
-    .catch(err => res.status(500).send(err));
-});
-
-// Update by url id
-router.put('/urlid/:urlId', (req, res) => {
-  Url.updateByUrlId(req.params.urlId, req.body)
-    .then(url => res.send(url))
-    .catch(err => res.status(500).send(err));
-});
-
-// Delete by url id
-router.delete('/urlid/:urlId', (req, res) => {
-  Url.deleteByUrlId(req.params.urlId)
-    .then(() => res.sendStatus(200))
-    .catch(err => res.status(500).send(err));
+  createShortUrl(res, req);
 });
 
 module.exports = router;
